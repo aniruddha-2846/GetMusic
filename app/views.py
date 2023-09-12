4b2a382a67982bcce5c1916d63a2cbdb462d1228
aniruddha-2846 <aniruddha2846@gmail.com>
2023-09-12 19:19:22 +0530

    with changes

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ import requests
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'trail.html')
 
 def runpyfile(request):
     if(request.method=='POST'):
@@ -53,17 +53,17 @@ def runpyfile(request):
             except Exception as e:
                 print(f"An error occurred: {str(e)}")
 
-            #click the pause button
+            #click the mute button
             try:
-                mute_button = wait.until(EC.element_to_be_clickable(
-                    (By.ID, 'player_volume')))
-                mute_button.click()
-
-                import time
-                time.sleep(30)
+            #     mute_button = wait.until(EC.element_to_be_clickable(
+            #         (By.ID, 'player_volume')))
+            #     mute_button.click()
+                    mute_button = driver.find_element(By.ID,'player_volume')
+                    mute_button.click()   
 
             except Exception as e:
-                print(f"An error occurredm while pausing: {str(e)}")                        
+                print(f"An error occurred while pausing: {str(e)}")   
+                              
 
             # get the audio url
             # required_url =''
@@ -87,6 +87,11 @@ def runpyfile(request):
                     print(f"Failed to download. Status code: {response.status_code}")
             except Exception as e:
                 print(f"An error occurred: {str(e)}")
-            return render(request, 'index.html')
+            finally:
+                driver.quit()
+                
+            return render(request, 'trail.html')
+        
+
             
                 
